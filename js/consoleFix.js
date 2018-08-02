6c52e3ee7ece52f945185a1da39e7edacebe9f4e
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-08-02 18:06:01 +0900

    published

---
+++ js/consoleFix.js
@@ -0,0 +1,63 @@
+/**
+ * @name consoleFix
+ * @author JungHyunKwon
+ * @since 2018-08-02
+ * @version 1.0.0
+ */
+if(!(window.console instanceof Object)) {
+	window.console = {
+		method : [
+			'assert',
+			'clear',
+			'count',
+			'debug',
+			'dir',
+			'dirxml',
+			'error',
+			'exception',
+			'group',
+			'groupCollapsed',
+			'groupEnd',
+			'info',
+			'log',
+			'markTimeline',
+			'profile',
+			'profileEnd',
+			'table',
+			'time',
+			'timeEnd',
+			'timeStamp',
+			'trace',
+			'warn'
+		],
+		comment : []
+	};
+
+	for(var i = 0, consoleMethodLength = window.console.method.length; i < consoleMethodLength; i++) {
+		//함수가아닐때
+		if(typeof window.console[window.console.method[i]] !== 'function') {
+			window.console[window.console.method[i]] = function() {
+				var result = [],
+					argumentsLength = arguments.length;
+			
+				//매개변수가 2개이상일때
+				if(argumentsLength > 1) {
+					for(var i = 0; i < argumentsLength; i++) {
+						result.push(arguments[i]);
+					}
+				
+				//매개변수가 한개일때
+				}else if(argumentsLength === 1) {
+					result = arguments[0];
+				}
+			   
+				//console.comment에 기입
+				if(argumentsLength) {
+					this.comment.push(result);
+				}
+
+				return result;
+			};
+		}
+	}
+}
