7b254085e13a5c3c7a9514ba6aebd6f44a5f9fe2
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-08-21 14:03:06 +0900

    published

--- js/consoleFix.js
+++ js/consoleFix.js
@@ -7,40 +7,86 @@
 
 'use strict';
 
-//객체일때
-if(!(window.console instanceof Object && window.console.constructor === Object)) {
-	window.console = {
-		method : ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'],
-		comment : []
-	};
+(function() {
+	var consoleType = getType(window.console);
 
-	for(var i = 0, consoleMethodLength = window.console.method.length; i < consoleMethodLength; i++) {
-		var methodI = window.console.method[i];
-
-		//함수가아닐때
-		if(typeof window.console[methodI] !== 'function') {
-			window.console[methodI] = function() {
-				var result = [],
-					argumentsLength = arguments.length;
+	/**
+	 * @name 형태얻기
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {string || undefined}
+	 */
+	function getType(value) {
+		var result;
+		
+		//매개변수가 있을때
+		if(arguments.length) {
+			//null일때
+			if(value === null) {
+				result = 'null';
 			
-				//매개변수가 2개이상일때
-				if(argumentsLength > 1) {
-					for(var i = 0; i < argumentsLength; i++) {
-						result.push(arguments[i]);
-					}
+			//undefined일때
+			}else if(value === undefined) {
+				result = 'undefined';
+			}else{
+				result = Object.prototype.toString.call(value).toLowerCase().replace('[object ', '').replace(']', '');
 				
-				//매개변수가 한개일때
-				}else if(argumentsLength === 1) {
-					result = arguments[0];
+				//Invalid Date일때
+				if(result === 'date' && isNaN(new Date(value))) {
+					result = 'Invalid Date';
+				
+				//숫자일때
+				}else if(result === 'number') {
+					//NaN일때
+					if(isNaN(value)) {
+						result = 'NaN';
+					
+					//Infinity일때
+					}else if(!isFinite(value)) {
+						result = value.toString();
+					}
 				}
-			   
-				//console.comment에 기입
-				if(argumentsLength) {
-					this.comment.push(result);
-				}
+			}
+		}
 
-				return result;
-			};
+		return result;
+	}
+
+	//객체가 아니거나 콘솔이 아닐때
+	if(consoleType !== 'object' || consoleType !== 'console') {
+		window.console = {
+			method : ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'],
+			comment : []
+		};
+
+		for(var i = 0, consoleMethodLength = window.console.method.length; i < consoleMethodLength; i++) {
+			var methodI = window.console.method[i];
+
+			//함수가아닐때
+			if(typeof window.console[methodI] !== 'function') {
+				window.console[methodI] = function() {
+					var result = [],
+						argumentsLength = arguments.length;
+				
+					//매개변수가 2개이상일때
+					if(argumentsLength > 1) {
+						for(var i = 0; i < argumentsLength; i++) {
+							result.push(arguments[i]);
+						}
+					
+					//매개변수가 한개일때
+					}else if(argumentsLength === 1) {
+						result = arguments[0];
+					}
+				   
+					//console.comment에 기입
+					if(argumentsLength) {
+						this.comment.push(result);
+					}
+
+					return result;
+				};
+			}
 		}
 	}
-}
+})();
