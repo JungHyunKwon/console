8cc1ec254cbfb7f298dcbb75a3b755d8363e5379
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-10-07 16:02:28 +0900

    published

--- js/consoleFix.js
+++ js/consoleFix.js
@@ -47,6 +47,14 @@
 				//콘솔일때
 				}else if(result === 'console') {
 					result = 'object';
+				
+				//요소일때
+				}else if(result.indexOf('element') > -1) {
+					result = 'element';
+				
+				//문서일때
+				}else if(result.indexOf('document') > -1) {
+					result = 'document';
 				}
 			}
 		}
