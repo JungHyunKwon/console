314bbeb3a0aba377b2ce6a00d2c91964e5010c69
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-10-26 23:33:58 +0900

    published

--- js/consoleFix.js
+++ js/consoleFix.js
@@ -9,7 +9,7 @@
 	'use strict';
 
 	/**
-	 * @name 형태얻기
+	 * @name 형태 얻기
 	 * @since 2017-12-06
 	 * @param {*} value
 	 * @return {string || undefined}
@@ -17,42 +17,42 @@
 	function _getType(value) {
 		var result;
 		
-		//매개변수가 있을때
+		//매개변수가 있을 때
 		if(arguments.length) {
-			//null일때
+			//null 일 때
 			if(value === null) {
 				result = 'null';
 			
-			//undefined일때
+			//undefined 일 때
 			}else if(value === undefined) {
 				result = 'undefined';
 			}else{
 				result = Object.prototype.toString.call(value).toLowerCase().replace('[object ', '').replace(']', '');
 				
-				//Invalid Date일때
+				//Invalid Date 일 때
 				if(result === 'date' && isNaN(new Date(value))) {
 					result = 'Invalid Date';
 				
-				//숫자일때
+				//숫자 일 때
 				}else if(result === 'number') {
-					//NaN일때
+					//NaN 일 때
 					if(isNaN(value)) {
 						result = 'NaN';
 					
-					//Infinity일때
+					//Infinity 일 때
 					}else if(!isFinite(value)) {
 						result = value.toString();
 					}
 				
-				//콘솔일때
+				//콘솔 일 때
 				}else if(result === 'console') {
 					result = 'object';
 				
-				//요소일때
+				//요소 일 때
 				}else if(result.indexOf('element') > -1) {
 					result = 'element';
 				
-				//문서일때
+				//문서 일 때
 				}else if(result.indexOf('document') > -1) {
 					result = 'document';
 				}
@@ -62,7 +62,7 @@
 		return result;
 	}
 
-	//객체가 아닐때
+	//객체가 아닐 때
 	if(_getType(console) !== 'object') {
 		var methodNames = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
 
@@ -73,19 +73,25 @@
 		for(var i = 0, methodsLength = methodNames.length; i < methodsLength; i++) {
 			var methodName = methodNames[i];
 
-			//함수가 아닐때
+			//함수가 아닐 때
 			if(typeof window.console[methodName] !== 'function') {
 				var methodCode = 'window.console[\'' + methodName + '\'] = function() {\n';
 
 				methodCode += '\tvar result = [],\n';
 				methodCode += '\t\targumentsLength = arguments.length;\n\n';
+
+				//매개변수가 두 개 이상일 때
 				methodCode += '\tif(argumentsLength > 1) {\n';
 				methodCode += '\t\tfor(var i = 0; i < argumentsLength; i++) {\n';
 				methodCode += '\t\t\tresult.push(arguments[i]);\n';
 				methodCode += '\t\t}\n';
+
+				//매개변수가 한 개일 때
 				methodCode += '\t}else if(argumentsLength === 1) {\n';
 				methodCode += '\t\tresult = arguments[0];\n';
 				methodCode += '\t}\n\n';
+
+				//매개변수가 있을 때
 				methodCode += '\tif(argumentsLength) {\n';
 				methodCode += '\t\tthis.replace.push({\n';
 				methodCode += '\t\t\tmethodName : \'' + methodName + '\',\n';
