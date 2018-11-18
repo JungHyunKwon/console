4a83253f4a2e08946049945f4b10b424fccbd429
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-11-19 02:19:16 +0900

    published

--- js/consoleFix.js
+++ js/consoleFix.js
@@ -9,12 +9,13 @@
 	'use strict';
 
 	//콘솔이 없을 때
-	if(!console) {
+	//if(!console) {
 		var methodNames = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'],
-			methodCode = '';
+			methodCode = '',
+			replacement = [];
 			
 		window.console = {
-			replacement : []
+			replacement : replacement
 		};
 
 		console = window.console;
@@ -22,30 +23,24 @@
 		for(var i = 0, methodsLength = methodNames.length; i < methodsLength; i++) {
 			var methodName = methodNames[i];
 
-			methodCode += 'console[\'' + methodName + '\'] = function() {\n';
-			methodCode += '    var result = [],\n';
-			methodCode += '        argumentsLength = arguments.length,\n';
-			methodCode += '        replacement = this.replacement;\n\n';
-
-			//매개변수가 두 개 이상일 때
+			methodCode += 'console.' + methodName + ' = function() {\n';
+			methodCode += '    var result = {\n';
+			methodCode += '        methodName : \'' + methodName + '\'\n';
+			methodCode += '    },\n';
+			methodCode += '    argumentsLength = arguments.length;\n\n';
 			methodCode += '    if(argumentsLength > 1) {\n';
+			methodCode += '        result.value = [];\n\n';
+			methodCode += '        var value = result.value;\n\n';
 			methodCode += '        for(var i = 0; i < argumentsLength; i++) {\n';
-			methodCode += '            result.push(arguments[i]);\n';
+			methodCode += '            value[i] = arguments[i];\n';
 			methodCode += '        }\n';
-
-			//매개변수가 한 개일 때
 			methodCode += '    }else if(argumentsLength === 1) {\n';
-			methodCode += '        result = arguments[0];\n';
+			methodCode += '        result.value = arguments[0];\n';
 			methodCode += '    }\n\n';
-
-			//매개변수가 있을 때
 			methodCode += '    if(argumentsLength) {\n';
-			methodCode += '        replacement.push({\n';
-			methodCode += '            methodName : \'' + methodName + '\',\n';
-			methodCode += '            value : result\n';
-			methodCode += '        });\n';
+			methodCode += '        replacement.push(result);\n';
 			methodCode += '    }\n\n';
-			methodCode += '    return replacement;\n';
+			methodCode += '    return result;\n';
 			methodCode += '};\n\n';
 		}
 		
@@ -54,5 +49,5 @@
 
 		//함수 기입
 		eval(methodCode);
-	}
+	//}
 })(window.console);
