11d77fbb584fbf83f618a31892a6d54d8bc32305
JungHyunKwon <kjh_3859@naver.com>
2020-01-28 21:20:27 +0900

    published

--- js/console.js
+++ js/console.js
@@ -9,39 +9,23 @@
 
 	//콘솔이 없을 때
 	if(!console) {
-		var methodNames = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'],
-			methodCode = '',
-			alt = [];
+		var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'],
+			alt = [],
+			slice = Array.prototype.slice;
 			
 		window.console = {
 			alt : alt
 		};
 
-		for(var i = 0, methodsLength = methodNames.length; i < methodsLength; i++) {
-			var methodName = methodNames[i];
-
-			methodCode += 'window.console.' + methodName + ' = function() {\n';
-			methodCode += '    var result = {\n';
-			methodCode += '        methodName : \'' + methodName + '\'\n';
-			methodCode += '    },\n';
-			methodCode += '    argumentsLength = arguments.length;\n\n';
-			methodCode += '    if(argumentsLength > 1) {\n';
-			methodCode += '        result.value = [];\n\n';
-			methodCode += '        var value = result.value;\n\n';
-			methodCode += '        for(var i = 0; i < argumentsLength; i++) {\n';
-			methodCode += '            value[i] = arguments[i];\n';
-			methodCode += '        }\n';
-			methodCode += '    }else if(argumentsLength === 1) {\n';
-			methodCode += '        result.value = arguments[0];\n';
-			methodCode += '    }\n\n';
-			methodCode += '    if(argumentsLength) {\n';
-			methodCode += '        alt.push(result);\n';
-			methodCode += '    }\n\n';
-			methodCode += '    return result;\n';
-			methodCode += '};\n\n';
+		for(var i = 0, methodsLength = methods.length; i < methodsLength; i++) {
+			(function(method) {
+				console[method] = function() {
+					alt.push({
+						method : method,
+						value : slice.call(arguments)
+					});
+				};			
+			})(methods[i]);
 		}
-
-		//함수 기입
-		eval(methodCode);
 	}
 })(window.console);
